f30207fc74679e9bad94b5736b1ccc03da32a54d
jess strobel <jjs8dzg@virginia.edu>
2023-07-02 18:40:27 -0400

    test to create table in models.py

--- database_sisters/search_engine/models.py
+++ database_sisters/search_engine/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+# jess db test
+# sorry that this has taken me genuinely 4 hours to figure out lmao i do not know how to use django at all
+class Student(models.Model):
+    student_id = models.CharField(max_length=200)
+    firstname = models.CharField(max_length=200)
+    lastname = models.CharField(max_length=200)
+
+    def _str_(self):
+        return self.student_id
