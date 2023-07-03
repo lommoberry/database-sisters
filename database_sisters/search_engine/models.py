9119946f87fb2302a713612f5027d7dd33623188
jess strobel <jjs8dzg@virginia.edu>
2023-07-03 10:29:53 -0400

    added tables in models.py: COUNTRY, DATE, JOURNAL_ENTRY, SITE, SKETCH

--- database_sisters/search_engine/models.py
+++ database_sisters/search_engine/models.py
@@ -1,19 +1,17 @@
 from django.db import models
 
-# # jess db test
-# # sorry that this has taken me genuinely 4 hours to figure out lmao i do not know how to use django at all
-# class Student(models.Model):
-#     student_id = models.CharField(max_length=200)
-#     firstname = models.CharField(max_length=200)
-#     lastname = models.CharField(max_length=200)
-#
-#     def _str_(self):
-#         return self.student_id
+'''
+project databases
 
-# project databases
-# initially created w/o constraints/foreign keys - will add in later
-# tables populated with sample data (via shell) for now
+TODO:
+- add constraints/foreign keys to all tables [pk, fk, autoincrement, delete cascade, not null, etc.]
+- correct max_length for models.CharField
+- create associative tables: AUTHOR_JOURNAL, JOURNAL_COUNTRY, SITE_ENTRY
+- populate tables with correct data (tables populated with sample data (via shell) for now)
+- figure out how to actually store an image via BlobField lmao
+
 # view tables: https://inloop.github.io/sqlite-viewer/ then drag and drop db.sqlite3
+'''
 
 
 class JOURNAL(models.Model):
@@ -24,9 +22,35 @@ class JOURNAL(models.Model):
     JOURNAL_TITLE = models.CharField(max_length=200)  # fix max length for all charfields later
 
 
+class COUNTRY(models.Model):
+    COUNTRY_ID = models.IntegerField()
+    COUNTRY_NAME = models.CharField(max_length=200)
+
+
+class JOURNAL_ENTRY(models.Model):
+    ENTRY_ID = models.IntegerField()
+    ENTRY_TEXT = models.CharField(max_length=1000)
+
+
+class SITE(models.Model):
+    SITE_ID = models.IntegerField()
+    SITE_NAME = models.CharField(max_length=200)
+
+
+class SKETCH(models.Model):
+    SKETCH_ID = models.IntegerField()
+    # SKETCH = models.BlobField()
+
+
+class DATE(models.Model):
+    DATE_FULL = models.IntegerField()
+    MONTH = models.IntegerField()
+    DAY = models.IntegerField()
+    YEAR = models.IntegerField()
+
+
 class AUTHOR(models.Model):
     AUTH_ID = models.IntegerField()
     AUTH_FNAME = models.CharField(max_length=200)
     AUTH_LNAME = models.CharField(max_length=200)
-
 
