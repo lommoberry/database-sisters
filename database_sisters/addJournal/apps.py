a22fa2c5101497f224b190451e51013852b60b80
Bridget Kim <xpt3bn@virginia.edu>
2023-07-07 12:03:23 -0400

    made addJOurnals

--- database_sisters/addJournal/apps.py
+++ database_sisters/addJournal/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AddjournalConfig(AppConfig):
+class AddJournalConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'addJournal'
--- database_sisters/addJournal/models.py
+++ database_sisters/addJournal/models.py
@@ -1,9 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class Journal(models.Model):
-    Journal_Title = models.CharField(max_length=100)
-    Num_Entries = models.IntegerField()
-    Century = models.CharField(max_length=2) #ex.) 19
-    Journal_ID = models.IntegerField(primary_key=True)
-
