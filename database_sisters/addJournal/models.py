8748665f4e134679cbb156035b9a131a7ba6a470
Bridget Kim <xpt3bn@virginia.edu>
2023-07-07 12:45:50 -0400

    made addJOurnals

--- database_sisters/addJournal/models.py
+++ database_sisters/addJournal/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django import forms
+from django.db import connection
 
 # Create your models here.
+class YourForm(forms.ModelForm):
+    with connection.cursor as cursor:
+        model = "journal"
+        fields = '__all__'
--- database_sisters/addJournal/views.py
+++ database_sisters/addJournal/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.db import connection
 
 # Create your views here.
+def add_journal_request(request):
+    return render(request, "editingdatabase/adding.html")
+
+
+def add_data(request):
+    if request.method == 'POST':
+        form = YourForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('success')  # Redirect to a success page
+    else:
+        form = YourForm()
+
+    return render(request, 'add_data.html', {'form': form})
