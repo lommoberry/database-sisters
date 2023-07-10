75bcc5407e784ec2181ec441f9a5d0723db6be26
anl8rpm <anl8rpm@virginia.edu>
2023-07-10 12:59:36 -0400

    Removed fields and updated journal view

--- database_sisters/search_engine/views.py
+++ database_sisters/search_engine/views.py
@@ -16,39 +16,50 @@ def results(request):
             filter_map[filter] = request.GET[filter]
 
         if table == "journal":
-            results, cols = journal_request(filter_map)
+            results, cols, atags = journal_request(filter_map)
         
         elif table == "journal_entry":
-            results, cols = journal_entry_request(filter_map)
+            results, cols, atags = journal_entry_request(filter_map)
 
         elif table =="author":
-            results, cols = author_request(filter_map)
+            results, cols, atags = author_request(filter_map)
 
         elif table == "sketch":
-            results, cols = sketch_request(filter_map)
+            results, cols, atags = sketch_request(filter_map)
 
         else:
             return render(request, "search_engine/results.html")    # illegal get request
         
-    return render(request, "search_engine/results.html", {"results": results,  "cols": cols})
+    return render(request, "search_engine/results.html", {"results": results,  "cols": cols, "atags": atags})
 
 
 def journal_request(filters):
     with connection.cursor() as cursor:
 
-        cols = ["id", "num_entries", "century", "title"]
+        cols = ["Title", "First", "Last", "Century", "Number of entries", "Country"]
+        atags = [0, 1, 2]
 
-        query = "SELECT * FROM journal" 
+        query = """SELECT journal.journal_title, author.auth_fname, author.auth_lname, journal.century, journal.num_entries, country.country_name
+                    FROM journal_country
+                    LEFT JOIN country
+                    ON journal_country.country_id = country.country_id
+                    LEFT JOIN journal
+                    ON journal_country.journal_id = journal.journal_id
+                    LEFT JOIN author_journal
+                    ON journal.journal_id=author_journal.journal_id
+                    LEFT JOIN author
+                    ON author.auth_id=author_journal.auth_id;""" 
 
         cursor.execute(query)
         results = cursor.fetchall()
 
-    return results, cols
+    return results, cols, atags
 
 def journal_entry_request(filters):
     with connection.cursor() as cursor:
 
         cols = ["entry_id", "journal_id", "text", "date_full", "sketch_id"]
+        atags = []
 
 
         query = "SELECT * FROM journal_entry"
@@ -56,13 +67,14 @@ def journal_entry_request(filters):
         cursor.execute(query)
         results = cursor.fetchall()
 
-    return results, cols
+    return results, cols, atags
     
 def author_request(filters):
     with connection.cursor() as cursor:
 
         params = []
         cols = ["First", "Last", "Country", "Journal"]
+        atags = []
 
         query = """SELECT author.auth_fname, author.auth_lname, country.country_name, journal.journal_title
                     FROM author_journal 
@@ -86,17 +98,18 @@ def author_request(filters):
         cursor.execute(query, params)
         results = cursor.fetchall()
 
-    return results, cols
+    return results, cols, atags
 
 def sketch_request(filters):
     with connection.cursor() as cursor:
 
         cols = ["sketch_id", "sketch"]
+        atags = []
 
         query = "SELECT * FROM sketch"
 
         cursor.execute(query)
         results = cursor.fetchall()
 
-    return results, cols
+    return results, cols, atags
 
